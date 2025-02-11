b142b34f8748e218db251d3f5a66484a5afbd492
larissa de Oliveira <larissaoliveira3zb@gmail.com>
2025-02-11 14:31:31 -0300

    Construindo a página Program

--- Program.cs
+++ Program.cs
@@ -8,7 +8,34 @@ class Program
         while (true)
         {
             Formatacao.ImprimirCabecalho();
-            
+            Formatacao.Cor("\n1 - Adicionar Tarefa", ConsoleColor.White);
+            Formatacao.Cor("2 - Listar Tarefas", ConsoleColor.White);
+            Formatacao.Cor("3 - Concluir Tarefa", ConsoleColor.White);
+            Formatacao.Cor("4 - Remover Tarefa", ConsoleColor.White);
+            Formatacao.Cor("0 - Sair", ConsoleColor.White);
+            Formatacao.Cor("\nEscolha uma opção: ");
+
+            string opcao = Console.ReadLine();
+
+            switch (opcao)
+            {
+                case "1":
+                Formatacao.Cor("\nDigite a descrição da Tarefa: ", ConsoleColor.White);
+                string descricao = Console.ReadLine
+                GerenciadorTarefas.AdicionarTarefa(descricao);
+                break;
+
+                case "2":
+                Formatacao.Cor("\nTarefas: \n", ConsoleColor.White);
+                GerenciadorTarefas.ListarTarefas();
+                break;
+
+                case "3":
+                Formatacao.Cor("\nDigite o Id da Tarefa que deseja concluir: ", ConsoleColor.White);
+                if (int.TryParse(Console.ReadLine(), out int idConcluir))
+                GerenciadorTarefas.ConcluirTarefa(idConcluir);
+                break;
+            }
         }
     }
 }
